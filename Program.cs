49ea46ce3ea11257a0d397129eb757ad665ba293
sarvesh.upadhyay <sarveshupadhayay@tavant.com>
2021-04-19 12:58:32 +0530

    adding LeaderFollower sample project to git

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using RaftImplementation;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LeaderFollowerPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey key = ConsoleKey.Enter;
+            Console.WriteLine("please press Escape to exit");
+            key = Console.ReadKey().Key;
+            var processors = new List<OrderEventProcessor>();
+            while(key != ConsoleKey.Escape)
+            {
+                if (key == ConsoleKey.S)
+                {
+                    if (processors.Count > 0)
+                    {
+                        var leader = processors.Find(p => p.State.GetType() == typeof(Leader));
+                        leader.Stop();
+                    }
+                }
+                else
+                {
+                    var orderEventProcessor1 = new OrderEventProcessor();
+                    orderEventProcessor1.Start();
+                    processors.Add(orderEventProcessor1);
+                    Task.Delay(1000);
+                    var orderEventProcessor2 = new OrderEventProcessor();
+                    orderEventProcessor2.Start();
+                    processors.Add(orderEventProcessor2);
+                    Task.Delay(1000);
+                    var orderEventProcessor3 = new OrderEventProcessor();
+                    orderEventProcessor3.Start();
+                    processors.Add(orderEventProcessor3);
+                }
+                Console.WriteLine("please press Escape to exit and [S]top Leader");
+                
+                key = Console.ReadKey().Key;
+
+            }
+        }
+    }
+}
